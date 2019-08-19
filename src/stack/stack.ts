9f73ef47cef6502b4da754ffd77f7c53aabac458
Sanket Maru <sanketmaru@gmail.com>
2019-08-19 19:45:08 +0530

    esdocs added

--- src/stack/stack.ts
+++ src/stack/stack.ts
@@ -1,6 +1,9 @@
 // Implement a Stack using Array. Your task is to complete the functions below.
 // You are required to complete two methods push which take one argument an integer 'x' to be pushed into the stack
 // and pop which returns a integer poped out from the stack.
+/**
+ * this is Stack Class.
+ */
 export class Stack {
     public top: number;
     public arr: number[] = [];
@@ -8,8 +11,10 @@ export class Stack {
         this.top = -1;
     }
 
-    /* The method push to push element
-	    into the stack */
+    /** The method push to push element into the stack
+     * @param {number} a - this is a value.
+     * @return {void}
+     */
     public push(a: number) {
         if (!a) {
             // tslint:disable-next-line: no-console
